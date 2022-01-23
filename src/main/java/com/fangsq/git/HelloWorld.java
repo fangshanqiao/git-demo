c7fde623d8607d652b98482e0978dd6d8276797a
fangsq <3230567149@qq.com>
2022-01-23 23:52:37 +0800

    second commit git

--- src/main/java/com/fangsq/git/HelloWorld.java
+++ src/main/java/com/fangsq/git/HelloWorld.java
@@ -3,5 +3,6 @@ package com.fangsq.git;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
